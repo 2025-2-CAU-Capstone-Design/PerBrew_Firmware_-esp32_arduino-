726485c45b20ca276cf5f4d11d2b027c3631eaad
sweetbolly <swjjaang@naver.com>
2025-10-10 21:31:17 +0900

    Add missing header file(arranging_driver)

---
+++ driver/arranging/arranging_driver.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <Arduino.h>
+#include <AccelStepper.h>
+#include "../pin_map.h"
+
+class ArrangingDriver {
+public:
+    ArrangingDriver();
+
+    void begin();
+    void setDirection(bool clockwise);  // 방향 설정 (true: 시계, false: 반시계)
+    void move(long steps = 200);        // 현재 방향으로 이동
+    void update();                      // 메인 루프에서 반복 호출
+
+private:
+    AccelStepper stepper_;
+    bool directionCW_;                  // true = 시계 방향
+};
